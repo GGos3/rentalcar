09f59ee8571f572e78ff912bcae2ffe109bec986
GGos3 <dev.ggos3@gmail.com>
2024-01-10 16:59:02 +0900

    chore: init

---
+++ src/test/java/com/ggos3/rentalcar/RentalCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggos3.rentalcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
